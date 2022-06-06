c5bf4d368105e55ff866af0d4b7be5e2a38adcff
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2022-06-06 18:52:09 +0500

    today front end changes

--- client/src/components/pages/ForgotPassword/index.tsx
+++ client/src/components/pages/ForgotPassword/index.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import { Avatar, CssBaseline, TextField, Box, Typography, Container, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import HomeLayout from "../../layout/HomeLayout";
 
 const ForgotPassword = () => {
