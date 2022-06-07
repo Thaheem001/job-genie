6ddf74578ade4ac4893a96e357dee6bed4271d01
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2022-06-07 15:46:32 +0500

    Register Page Fetch Api

--- client/src/components/pages/PaymentSuccess/index.tsx
+++ client/src/components/pages/PaymentSuccess/index.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
-
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ScreenLoader from "../../shared/ScreenLoader";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 type Props = {};
 
 const PaymentSuccess = (props: Props) => {
   const { paymentTokken } = useParams();
+  const [isVisibile, setIsVisible] = useState<boolean>(true);
 
   const verifyPayment = async () => {
     try {
-      // const dataSnap = await fetch(`${origin}/api`);
       const dataSnap = await fetch("/api/paymentSuccess", {
         method: "post",
         headers: { "Content-Type": "application/json" },
@@ -22,20 +23,29 @@ const PaymentSuccess = (props: Props) => {
         alert(data.error);
         throw new Error(data.error);
       }
-
+      setIsVisible(false)
       return console.log("data is -->", data);
     } catch (error) {
       console.log("something weird happend", error);
     }
   };
 
-  //   navigate("/");
 
   useEffect(() => {
     verifyPayment();
   }, []);
 
-  return <div>PaymentSuccess</div>;
+  return <>
+    <ScreenLoader isVisible={isVisibile} />
+    {
+      !isVisibile &&
+      <div className="paymentSuccess">
+        <CheckCircleIcon />
+        <h1 className="text-light">Payment has been Successfully Deposite.</h1>
+        <Link className="btn-own" to={`/home`}>Go to Dashboard</Link>
+      </div>
+    }
+  </>;
 };
 
 export default PaymentSuccess;
