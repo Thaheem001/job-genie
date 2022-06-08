adc96086f8ace82e66dd92385bd1b13677cbf947
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2022-06-08 11:09:41 +0500

    update package

--- client/src/components/pages/Dashboard/index.tsx
+++ client/src/components/pages/Dashboard/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 
 type Props = {}
 
 const DashboardPage = (props: Props) => {
+    const notify = () => toast.loading('This is an error!');
     return (
-        <div>DashboardPage</div>
+        <>
+            <h1 className="text-center text-light">DashboardPage</h1>
+            <button onClick={notify}>Make me a toast</button>
+            <Toaster />
+        </>
     )
 }
 
