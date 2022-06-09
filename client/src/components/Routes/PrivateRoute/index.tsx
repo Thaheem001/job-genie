d79e9deb897e99038ebb2eb2d0f10460a7eaac87
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2022-06-09 16:38:51 +0500

    Today 6/09/2022

--- client/src/components/Routes/PrivateRoute/index.tsx
+++ client/src/components/Routes/PrivateRoute/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import ScreenLoader from "../../shared/ScreenLoader";
 import { validateAuthCookie } from "../../utils/validateAuthCookie";
@@ -10,23 +9,17 @@ type Props = {};
 const PrivateRoute = (props: Props) => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(true);
-  const userLoginState = useSelector(state => state);
-  const dispatch = useDispatch();
 
   useEffect(() => {
     validateAuthCookie()
       .then((isValid) => {
-        if (!userLoginState) {
-          if (!isValid) {
-            throw new Error("Tokken Expired!");
-          }
+        if (!isValid) {
+          throw new Error("Tokken Expired!");
         }
-        dispatch({ type: 'user_verify', payload: true });
         setLoading(false);
         toast.success("Login SuccessFul");
       })
       .catch((error: string) => {
-        dispatch({ type: 'user_verify_false', payload: false });
         toast.error(error.toString());
         navigate("/login");
       });
