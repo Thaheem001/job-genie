157b698daf7a1deb2540dc2d56a45e4ccdfad4cc
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2022-06-09 16:37:45 +0500

    Today 6/09/2022

--- client/src/components/Routes/PrivateRoute/index.tsx
+++ client/src/components/Routes/PrivateRoute/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
 import ScreenLoader from "../../shared/ScreenLoader";
 import { validateAuthCookie } from "../../utils/validateAuthCookie";
@@ -9,17 +10,23 @@ type Props = {};
 const PrivateRoute = (props: Props) => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(true);
+  const userLoginState = useSelector(state => state);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     validateAuthCookie()
       .then((isValid) => {
-        if (!isValid) {
-          throw new Error("Tokken Expired!");
+        if (!userLoginState) {
+          if (!isValid) {
+            throw new Error("Tokken Expired!");
+          }
         }
+        dispatch({ type: 'user_verify', payload: true });
         setLoading(false);
         toast.success("Login SuccessFul");
       })
       .catch((error: string) => {
+        dispatch({ type: 'user_verify_false', payload: false });
         toast.error(error.toString());
         navigate("/login");
       });
--- client/src/components/pages/Dashboard/Challenges/index.tsx
+++ client/src/components/pages/Dashboard/Challenges/index.tsx
@@ -6,9 +6,12 @@ type Props = {}
 const Challenges = (props: Props) => {
     return (
         <>
-        <DashboardLayout>
-            <h1 className="text-center text-light">Chellenge</h1>
-        </DashboardLayout>
+            <DashboardLayout>
+                <>
+                    <h1 className="text-center text-light">Chellenge</h1>
+                    
+                </>
+            </DashboardLayout>
         </>
     )
 }
