7d97b3acb169c60b9b549b6cc3b38b8f3d87fbe0
= <=>
2022-06-13 13:26:01 +0500

    integrated redux toolkit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,7 +9,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
