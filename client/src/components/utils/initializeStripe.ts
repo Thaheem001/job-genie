ea741d68e200b23ad4691ee164fcf6ebc58e9fd4
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2022-06-15 18:29:55 +0500

    changes

--- client/src/components/utils/initializeStripe.ts
+++
@@ -1,7 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-const initializeStripe = async () => {
-  return await loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || "");
-};
-
-export default initializeStripe;
